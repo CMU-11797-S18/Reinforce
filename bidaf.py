b661ce71a41b3299f4282cd91c7412423761cc08
Prashant Budania <prashantbudania250@gmail.com>
2018-03-17 21:13:20 -0400

    Baseline Implementation

--- bidaf.py
+++ bidaf.py
@@ -1,120 +1,168 @@
 #BIDAF Implementation
-import dynet as dy, numpy as np
+import numpy as np
+import dynet as dy
 from time import time
+import json
 	
-
 class BiDAF():
-	def __init__(self, pc, word_emb_dim, hidden_dim):
-		self.pc = pc
-		self.word_emb_dim = word_emb_dim
-		self.hidden_dim = hidden_dim
-		
-		self.lookup_table = pc.add_lookup_parameters((100000, word_emb_dim)) # or use pretrained glove embeddings
-		self.W_ss_ = dy.add_parameters((1, 3*hidden_dim))
-		self.b_ss_ = dy.add_parameters((1))
+    def __init__(self, pc, word_emb_dim, hidden_dim):
+        self.pc = pc
+        self.word_emb_dim = word_emb_dim
+        self.hidden_dim = hidden_dim
 
-		self.contextLSTM = dy.VannillaLSTMBuilder(1, word_emb_dim, hidden_dim, pc)
-		self.queryLSTM = dy.VannillaLSTMBuilder(1, word_emb_dim, hidden_dim, pc)
-		self.modellingLSTM = dy.VannillaLSTMBuilder(2, hidden_dim + some_dim + some_dim, hidden_dim, pc) #TODO
-		self.outputLSTM = dy.VannillaLSTMBuilder(1,,hidden_dim, pc)
+        #self.lookup_table = pc.add_lookup_parameters((100000, word_emb_dim)) # or use pretrained glove embeddings
+        self.W_ss_ = pc.add_parameters((1, 3*hidden_dim))
+        self.b_ss_ = pc.add_parameters((1))
+        self.W_p1_ = pc.add_parameters((1, 5*hidden_dim))
+        self.b_p1_ = pc.add_parameters((1))
+        self.W_p2_ = pc.add_parameters((1, 5*hidden_dim))
+        self.b_p2_ = pc.add_parameters((1))
 
-	def similarity_score(self,h,u):
-		concat = dy.concatenate([h,u,dy.cmult(h,u)],d=0)
-		score = self.W_ss * concat + self.b_ss
+        self.contextLSTM = dy.VanillaLSTMBuilder(1, word_emb_dim, hidden_dim, pc)
+        self.queryLSTM = dy.VanillaLSTMBuilder(1, word_emb_dim, hidden_dim, pc)
+        self.modellingLSTM = dy.VanillaLSTMBuilder(1, 4 * hidden_dim, hidden_dim, pc) #TODO
+        self.outputLSTM = dy.VanillaLSTMBuilder(1,hidden_dim,hidden_dim, pc)
 
-		return score
+    def similarity_score(self,h,u):
+        concat = dy.concatenate([h,u,dy.cmult(h,u)],d=0)
+        score = self.W_ss * concat + self.b_ss
 
-	def c2q_attention(self, sim_matrix, query_states):
-		attention_vector = dy.softmax(sim_matrix)
-		c2q = [dy.esum([a * b for a,b in zip(attention_vector[:,i], query_states)]) for i in range(self.T)]
+        return score.scalar_value()
 
-		return c2q
+    def c2q_attention(self, sim_matrix, query_states):
+        attention_vector = dy.softmax(sim_matrix)
+        c2q = [dy.esum([b * dy.select_cols(attention_vector,[i])[j]  for j,b in enumerate(query_states)]) for i in range(self.T)]
 
-	def q2c_attention(self, sim_matrix, context_states):
-		attention_vector = dy.softmax(dy.max_dim(sim_matrix,d=1))
-		weighted_vectors = [a * b for a,b in zip(attention_vector, context_states)]
+        return c2q
 
-		return [dy.esum(weighted_vectors) for _ in range(self.T)]
+    def q2c_attention(self, sim_matrix, context_states):
+        attention_vector = dy.softmax(dy.max_dim(sim_matrix,d=1))
+        weighted_vectors = [b * a for a,b in zip(attention_vector, context_states)]
 
-	def similarity_matrix(self, context_states, query_states):
-		sim_matrix = dy.zeros((self.J,self.T))
-		for i in range(len(query_states)):
-			for j in range(len(context_states)):
-				sim_matrix[i][j] = self.similarity_score(context_states[j],query_states[i])
+        return [dy.esum(weighted_vectors) for _ in range(self.T)]
 
-		return sim_matrix
+    def similarity_matrix(self, context_states, query_states):
+        sim_matrix = np.zeros((self.J,self.T))
+        for i in range(len(query_states)):
+            for j in range(len(context_states)):
+                sim_matrix[i][j] = self.similarity_score(context_states[j],query_states[i])
 
-	def span_scores(self,combined_input1, combined_input2):
-		p1 = dy.softmax(self.W_p1*combined_input1 + self.b_p1)
-		p2 = dy.softmax(self.W_p2*combined_input2 + self.b_p2)
+        return dy.inputTensor(sim_matrix)
 
-		return p1, p2
+    def span_scores(self,combined_input1, combined_input2):
+        p1 = [self.W_p1*combined_input1[i] + self.b_p1 for i in range(self.T)]
+        p2 = [self.W_p2*combined_input2[i] + self.b_p2 for i in range(self.T)]
+#         p2 = self.W_p2*dy.inputTensor(combined_input2) + self.b_p2
+        return p1, p2
 
 
-	def complete_forward_pass(self, inputs):
-		dy.renew_cg()
-		context = inputs[0]
-		query = inputs[1]
+    def complete_forward_pass(self, inputs):
+        dy.renew_cg()
+        context_embs = inputs[0]
+        query_embs = inputs[1]
 
-		self.T = len(context)
-		self.J = len(query)
+        query_embs = []
+        context_embs = []
 
-		self.W_ss = dy.parameter(self.W_ss_)
-		self.b_ss = dy.parameter(self.b_ss_)
-		self.W_p1 = dy.parameter(self.W_p1_)
-		self.b_p1 = dy.parameter(self.b_p1_)
-		self.W_p2 = dy.parameter(self.W_p2_)
-		self.b_p2 = dy.parameter(self.b_p2_)
+        for word in inputs[1]:
+            try:
+                query_embs.append(dy.inputTensor(word2vec[word.lower()]))
+            except:
+                query_embs.append(dy.random_normal(100))
 
-		contextLSTM_init = self.contextLSTM.initial_state()
-		queryLSTM_init = self.queryLSTM.initial_state()
-		modellingLSTM_init = self.contextLSTM.initial_state()
-		outputLSTM_init = self.queryLSTM.initial_state()
+        for word in inputs[0]:
+            try:
+                context_embs.append(dy.inputTensor(word2vec[word.lower()]))
+            except:
+                context_embs.append(dy.random_normal(100))
 
-		context_embs = [dy.lookup[self.lookup_table,x] for x in context]
-		query_embs = [dy.lookup[self.lookup_table,x] for x in query]
+        self.T = len(context_embs)
+        self.J = len(query_embs)
 
-		context_states = contextLSTM_init.transduce(context_embs)
-		query_states = queryLSTM_init.transduce(query_embs)
+        self.W_ss = dy.parameter(self.W_ss_)
+        self.b_ss = dy.parameter(self.b_ss_)
+        self.W_p1 = dy.parameter(self.W_p1_)
+        self.b_p1 = dy.parameter(self.b_p1_)
+        self.W_p2 = dy.parameter(self.W_p2_)
+        self.b_p2 = dy.parameter(self.b_p2_)
 
-		sim_matrix = self.similarity_matrix(context_states, query_states)
-		c2q = self.c2q_attention(sim_matrix, query_states)
-		q2c = self.q2c_attention(sim_matrix, context_states)
+        contextLSTM_init = self.contextLSTM.initial_state()
+        queryLSTM_init = self.queryLSTM.initial_state()
+        modellingLSTM_init = self.modellingLSTM.initial_state()
+        outputLSTM_init = self.outputLSTM.initial_state()
 
-		modelling_input = [dy.concatenate([context_states[i],c2q[i],
-						   dy.cmult(context_states[i],c2q[i]),
-						   dy.cmult(context_states[i],q2c[i])], d=0) for i in range(self.T)]
+        #context_embs = [dy.lookup[self.lookup_table,x] for x in context]
+        #query_embs = [dy.lookup[self.lookup_table,x] for x in query]
 
-		modelling_output1 = modellingLSTM_init.transduce(modelling_input)
-		modelling_output2 = outputLSTM_init.transduce(modelling_output1)
+        context_states = contextLSTM_init.transduce(context_embs)
+        query_states = queryLSTM_init.transduce(query_embs)
 
-		combined_input1 = [dy.concatenate([modelling_input[i],modelling_output1[i]], d=0) for i in range(self.T)]
-		combined_input2 = [dy.concatenate([modelling_input[i],modelling_output2[i]], d=0) for i in range(self.T)]
+        sim_matrix = self.similarity_matrix(context_states, query_states)
+        c2q = self.c2q_attention(sim_matrix, query_states)
+        q2c = self.q2c_attention(sim_matrix, context_states)
 
-		p1, p2 = self.span_scores(combined_input1, combined_input2)
+        modelling_input = [dy.concatenate([context_states[i],c2q[i],
+                           dy.cmult(context_states[i],c2q[i]),
+                           dy.cmult(context_states[i],q2c[i])], d=0) for i in range(self.T)]
+        
+        modelling_output1 = modellingLSTM_init.transduce(modelling_input)
+        modelling_output2 = outputLSTM_init.transduce(modelling_output1)
 
-		return p1, p2
+        combined_input1 = [dy.concatenate([modelling_input[i],modelling_output1[i]], d=0) for i in range(self.T)]
+        combined_input2 = [dy.concatenate([modelling_input[i],modelling_output2[i]], d=0) for i in range(self.T)]
+
+        p1, p2 = self.span_scores(combined_input1, combined_input2)
+
+        return p1, p2
+
+def loss_fn(s1, s2, gold_answer):
+    T = len(s1)
+    p1 = np.zeros(T)
+    p2 = np.zeros(T)
+    
+    for i in range(T):
+        p1[i] = s1[i].scalar_value()
+        p2[i] = s2[i].scalar_value()
+        
+    loss = dy.pickneglogsoftmax(dy.inputTensor(p1), gold_answer[0]) + dy.pickneglogsoftmax(dy.inputTensor(p2), gold_answer[1])
+    return loss
+
 
 def main():
 
-	# some data reader function
-	
+	shared_train = json.load(open('shared_train.json'))
+	data_train = json.load(open('data_train.json'))	
+
+	word2vec = shared_train['lower_word2vec']
+	Questions = data_train['q']
+	passage_id = data_train['*x']
+	Passages = shared_train['x']
+	Answers = np.array(data_train['y'])[:,0,:,0]  #answer spans
 	pc = dy.Model()
 	trainer = dy.AdamTrainer(pc)
 
-	model = BiDAF(pc,300,150)
+	model = BiDAF(pc,100,50)
 
-	for k in range(num_examples):
-		question = Questions[k]
-		passage = Passages[k]
-		answer = Answers[k]
+	for epoch in range(10):
+		train_loss = 0
+		for k in range(len(Answers)):
+			
+			answer = Answers[k]
+			doc = Passages[passage_id[k][0]][passage_id[k][1]]
+			
+			p1, p2 = model.complete_forward_pass((doc, Questions[k]))
+			loss = loss_fn(p1, p2, answer)
+			train_loss += loss.scalar_value()
+			loss.backward()
+			trainer.update()
+		print("Epoch: {} || Training Loss: {}".format(epoch+1,train_loss/len(Answers)))
 
-		p1, p2 = model.complete_forward_pass((passage, question))
-		loss = loss_fn(p1, p2, answer)
-		loss.backward()
-		trainer.update()
 
-	accuracy = predict('train')
+
+	#accuracy = predict('train')
+
+if __name__ == '__main__':
+	main()
 
 
 
